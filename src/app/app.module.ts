7cd7c98e5782b3dffbcf4bff7f31e573b0559bac
mathsukamura <matheus.yuri.melo@gmail.com>
2023-01-26 17:45:14 -0300

    components and config

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { pokedexComponent } from './home/pokedex.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    pokedexComponent,
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
