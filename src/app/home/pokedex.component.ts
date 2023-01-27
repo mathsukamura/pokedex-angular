7003d947123f60a1510f1103318020ecdcdcd8f6
mathsukamura <matheus.yuri.melo@gmail.com>
2023-01-27 17:26:01 -0300

    connection API

--- src/app/home/pokedex.component.ts
+++ src/app/home/pokedex.component.ts
@@ -9,3 +9,19 @@ import { Component } from "@angular/core";
 export class pokedexComponent{
 
 }
+
+const namePokemon = document.querySelector('.pokemonName') as any ;
+
+const fetchpokemon =async (pokemon: string = "") => {
+  const APIResponse= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await APIResponse.json();
+  return data
+}
+
+const renderPokemon = async (pokemon: string ="") =>{
+  const data = await fetchpokemon(pokemon);
+  namePokemon.innerHTML= data.nome
+
+}
+
+renderPokemon('1')
