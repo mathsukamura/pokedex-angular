c1316f645c667ae280f8dfe962ada1cb7d142280
mathsukamura <matheus.yuri.melo@gmail.com>
2023-02-24 16:05:10 -0300

    Add Function to search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { pokedexComponent } from './home/pokedex.component';
+import { PokedexComponent } from './home/pokedex.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    pokedexComponent,
+    PokedexComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/pokedex.component.ts
+++ src/app/home/pokedex.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import {  Component,OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokedex',
@@ -6,22 +8,60 @@ import { Component } from "@angular/core";
   styleUrls: ['./pokedex.component.scss'],
 })
 
-export class pokedexComponent{
+export class PokedexComponent implements OnInit {
+
+  pokemonName: string = 'Loading...';
+  pokemonNumber: number = 1;
+  pokemonImage: string = "";
+  input: string = '';
+  searchPokemon: number = 1;
+
+  constructor(private http: HttpClient) {}
+
+  fetchPokemon(pokemon: number) {
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  }
+
+  renderPokemon(pokemon: string) {
+    this.pokemonName = 'Loading...';
+    this.pokemonNumber = 1;
+
+    this.fetchPokemon(parseInt(pokemon.toLowerCase())).subscribe(
+      data => {
+        this.pokemonImage = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+        this.pokemonName = data.name;
+        this.pokemonNumber = data.id;
+        this.input = '';
+        this.searchPokemon = data.id;
+      },
+      error => {
+        this.pokemonImage = "";
+        this.pokemonName = 'Not found :c';
+        this.pokemonNumber = 1;
+      }
+    );
+  }
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.renderPokemon(this.input);
+  }
+
+  onButtonPrevClick() {
+    if (this.searchPokemon > 1) {
+      this.searchPokemon -= 1;
+      this.renderPokemon(this.searchPokemon.toString());
+    }
+  }
+
+  onButtonNextClick() {
+    this.searchPokemon += 1;
+    this.renderPokemon(this.searchPokemon.toString());
+  }
+
+  ngOnInit() {
+    this.renderPokemon(this.searchPokemon.toString());
+  }
 
 }
 
-const namePokemon = document.querySelector('.pokemonName') as any ;
-
-const fetchpokemon =async (pokemon: string = "") => {
-  const APIResponse= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await APIResponse.json();
-  return data
-}
-
-const renderPokemon = async (pokemon: string ="") =>{
-  const data = await fetchpokemon(pokemon);
-  namePokemon.innerHTML= data.nome
-
-}
-
-renderPokemon('1')
